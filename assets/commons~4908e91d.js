b045a451cef9290da82acc6822a1d5c56f75fc94
owidbot <tech@ourworldindata.org>
2021-08-19 15:07:00 +0000

    Automated update owid/owid-grapher@fbed86297cf1df324c03e7ee52e925369b2c43d3

--- assets/commons~4908e91d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{474:function(n,w,o){}}]);
-//# sourceMappingURL=commons~4908e91d.js.map
--- grapher/embedCharts.js
+++ grapher/embedCharts.js
@@ -1,20 +1,9 @@
 const embedSnippet = () => {
-
-const coreStylesheets = [
-    'https://ourworldindata.org/assets/commons~34e3d95a.css',
-'https://ourworldindata.org/assets/commons~4908e91d.css',
-'https://ourworldindata.org/assets/commons~7efcf74f.css',
-'https://ourworldindata.org/assets/commons~e4dfd1bd.css'
-]
-
-
-coreStylesheets.forEach(url => {
-    const link = document.createElement('link')
-    link.type = 'text/css'
-    link.rel = 'stylesheet'
-    link.href = url
-    document.head.appendChild(link)
-})
+const link = document.createElement('link')
+link.type = 'text/css'
+link.rel = 'stylesheet'
+link.href = 'https://ourworldindata.org/assets/commons.css'
+document.head.appendChild(link)
 
 let loadedScripts = 0;
 const checkReady = () => {
@@ -25,20 +14,8 @@ const checkReady = () => {
 
 const coreScripts = [
     'https://polyfill.io/v3/polyfill.min.js?features=es6,fetch,URL,IntersectionObserver,IntersectionObserverEntry',
-    'https://ourworldindata.org/assets/commons~17c351d0.js',
-'https://ourworldindata.org/assets/commons~1814676f.js',
-'https://ourworldindata.org/assets/commons~1d14d743.js',
-'https://ourworldindata.org/assets/commons~34e3d95a.js',
-'https://ourworldindata.org/assets/commons~4908e91d.js',
-'https://ourworldindata.org/assets/commons~6bf23d7e.js',
-'https://ourworldindata.org/assets/commons~7008702c.js',
-'https://ourworldindata.org/assets/commons~7efcf74f.js',
-'https://ourworldindata.org/assets/commons~b3c4a2ff.js',
-'https://ourworldindata.org/assets/commons~bc7ea7ee.js',
-'https://ourworldindata.org/assets/commons~d91c6bc2.js',
-'https://ourworldindata.org/assets/commons~e4dfd1bd.js',
-'https://ourworldindata.org/assets/commons~f075b844.js',
-'https://ourworldindata.org/assets/owid.js'
+    'https://ourworldindata.org/assets/commons.js',
+    'https://ourworldindata.org/assets/owid.js'
 ]
 
 coreScripts.forEach(url => {
